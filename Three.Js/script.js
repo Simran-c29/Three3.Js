61f6c8cd07f2fed1de0b82a85af49c36f9abeeaf
Simran-c29 <simranchhabra699@gmail.com>
2025-03-03 13:05:21 +0530

    Initialized the repo

---
+++ Three.Js/script.js
@@ -0,0 +1,13 @@
+/* default hover effect */
+a:hover {
+    color: black;
+    background: yellow;
+  }
+  
+  @media (hover: hover) {
+    /* when hover is supported */
+    a:hover {
+      color: white;
+      background: black;
+    }
+  }
